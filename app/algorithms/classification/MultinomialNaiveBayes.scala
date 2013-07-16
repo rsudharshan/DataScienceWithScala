5ca4e2785c3563cb4c0bb0e1466dcd1f58529fe7
sudhar <sudharshan.cbe@gmail.com>
2013-07-16 13:17:54 +0530

    Algorithms package added

---
+++ app/algorithms/classification/MultinomialNaiveBayes.scala
@@ -0,0 +1,15 @@
+package algorithms.classification
+
+/**
+ * User: Sudharshan
+ * Date: 16/7/13
+ * Time: 12:52 PM
+ */
+object MultinomialNaiveBayes {
+
+  def main(x:Array[String])
+  {
+    println("Multinomial naive bayes")
+  }
+
+}
